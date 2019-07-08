015f88b7bd54edd31bdf8028eb3eb0ac44a3dd76
jjwly <47018812+jjwly@users.noreply.github.com>
2019-07-08 15:07:52 +0800

    initial solution

---
+++ ProblemsAndSolutions/KillExcel/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        public static void Kill(Microsoft.Office.Interop.Excel.Application excel)
+        {
+            IntPtr t = new IntPtr(excel.Hwnd);//得到这个句柄，具体作用是得到这块内存入口 
+
+            int k = 0;
+            GetWindowThreadProcessId(t, out k);   //得到本进程唯一标志k
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(k);   //得到对进程k的引用
+            p.Kill();     //关闭进程k
+        }
+
+        static void Main(string[] args)
+        { 
+            Application application = new ApplicationClass();
+
+            if (application != null)
+            {
+                Kill(application);
+            }
+            Console.ReadLine();
+        }
+    }
+}
